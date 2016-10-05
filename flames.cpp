552c013fce0e5e1a78127b2257cbadd4f7796d4f
VonPro <vs_jabbawockeez21@outlook.com>
2016-10-05 11:10:51 +0800

    Create flames.cpp

---
+++ flames.cpp
@@ -0,0 +1,84 @@
+/**
+  * Copyright to VonPro (2016).
+  */
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int alphaCount(char name[30], char letter) {
+	int count = 0;
+
+	for (int x=0; x<strlen(name); x++) {
+   	if (name[x] == letter) {
+      	count++;
+      }
+   }
+
+   return count;
+}
+
+main(void) {
+	const char alphabet[] ={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char yourName[30], someName[30];
+
+   printf("Enter your name: ");
+   scanf("%s", &yourName);
+
+   printf("Enter someone's name: ");
+   scanf("%s", &someName);
+
+   // int maxLength = (strlen(yourName) > strlen(someName)) ? strlen(yourName) : strlen(someName);
+   int counter = 0;
+   int yourNameAlphaCount = 0, someNameAlphaCount = 0, totalAlphaCount = 0;
+   // bool isAlphaPresent = false;
+
+   do {
+   	yourNameAlphaCount = alphaCount(yourName, alphabet[counter]);
+      someNameAlphaCount = alphaCount(someName, alphabet[counter]);
+
+      if (yourNameAlphaCount > 0 && someNameAlphaCount > 0) {
+      	totalAlphaCount += (yourNameAlphaCount + someNameAlphaCount);
+         counter++;
+      }
+      else {
+      	counter++;
+      }
+   }	while (counter < strlen(alphabet));
+
+   char flames[6] = {'f', 'l', 'a', 'm', 'e', 's'};
+   // int flamesCounter = 0, flamesValue;
+
+   do {
+   	if (totalAlphaCount > strlen(flames) - 1) {
+      	totalAlphaCount -= 6;
+      }
+      else {
+      	printf("FLAMES result: ");
+      	switch (totalAlphaCount - 1) {
+         	case 0:
+            	printf("Friends");
+               break;
+            case 1:
+            	printf("Lovers");
+               break;
+            case 2:
+            	printf("Acquintance");
+               break;
+            case 3:
+            	printf("Marriage");
+               break;
+            case 4:
+            	printf("Enemy");
+               break;
+            case 5:
+            	printf("Sex :^)");
+               break;
+            default:
+            	printf("Something went wrong.");
+      	}
+         break;
+      }
+   }	while (true);
+
+   getche();
+}
